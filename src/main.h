dbab170e8f49168f1f632bc3daee51ffbf5c6288
TimSousa <martimnevessousa@gmail.com>
2024-01-03 01:28:23 +0000

    writing to file

---
+++ src/main.h
@@ -0,0 +1,31 @@
+#ifndef T_RENDER
+#define T_RENDER
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BIT_DEPTH 255
+
+typedef struct {
+    int x;
+    int y;
+} Vector2;
+
+typedef struct {
+    int r;
+    int g;
+    int b;
+} Color;
+
+typedef struct {
+    Color **pixel_array;
+
+    int width;
+    int height;
+} Image;
+
+int render_image(Image, char *out_filename);
+void free_image(Image image);
+
+int convert(float conv, Vector2 init, Vector2 final);
+#endif
---
+++ src/scene.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+int setup_scene(void){
+    return 0;
+}
